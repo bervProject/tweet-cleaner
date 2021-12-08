3a7dea0cb703f279102333e5c1911bacd4621b21
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-12-08 19:19:07 +0700

    Change net6 template

--- TweetCleaner/Program.cs
+++ TweetCleaner/Program.cs
@@ -1,68 +1,58 @@
-﻿using System;
-using System.Threading.Tasks;
-using Tweetinvi;
+﻿using Tweetinvi;
 
-namespace TweetCleaner
+var startTime = DateTimeOffset.UtcNow;
+var userClient = new TwitterClient(Environment.GetEnvironmentVariable("CONSUMER_KEY"),
+    Environment.GetEnvironmentVariable("CONSUMER_SECRET"),
+    Environment.GetEnvironmentVariable("ACCESS_TOKEN"),
+    Environment.GetEnvironmentVariable("ACCESS_SECRET"));
+var deletedTweetTask = CleanMyTimeLine(userClient);
+var unfavTweetTask = Unfav(userClient);
+Task.WaitAll(deletedTweetTask, unfavTweetTask);
+Console.WriteLine($"Deleted {deletedTweetTask.Result}, unfav {unfavTweetTask.Result}");
+var endTime = DateTimeOffset.UtcNow;
+Console.WriteLine($"Elapsed time: {(endTime - startTime).TotalSeconds} s");
+
+
+static async Task<int> CleanMyTimeLine(ITwitterClient userClient)
 {
-    class Program
+    var user = await userClient.Users.GetAuthenticatedUserAsync();
+    var tweets = await userClient.Timelines.GetUserTimelineAsync(user);
+    Console.WriteLine($"Getting tweets: {tweets.Length}");
+    int deletedTweet = 0;
+    foreach (var tweet in tweets)
     {
-        static void Main(string[] args)
+        if (!string.IsNullOrWhiteSpace(tweet.InReplyToScreenName) && !user.ScreenName.Equals(tweet.InReplyToScreenName))
         {
-            var startTime = DateTimeOffset.UtcNow;
-            var userClient = new TwitterClient(Environment.GetEnvironmentVariable("CONSUMER_KEY"),
-                Environment.GetEnvironmentVariable("CONSUMER_SECRET"),
-                Environment.GetEnvironmentVariable("ACCESS_TOKEN"),
-                Environment.GetEnvironmentVariable("ACCESS_SECRET"));
-            var deletedTweetTask = CleanMyTimeLine(userClient);
-            var unfavTweetTask = Unfav(userClient);
-            Task.WaitAll(deletedTweetTask, unfavTweetTask);
-            Console.WriteLine($"Deleted {deletedTweetTask.Result}, unfav {unfavTweetTask.Result}");
-            var endTime = DateTimeOffset.UtcNow;
-            Console.WriteLine($"Elapsed time: {(endTime - startTime).TotalSeconds} s");
+            Console.WriteLine($"Skip reply to another person: ${tweet.Text}");
+            continue;
         }
+        Console.WriteLine($"Tweet: {tweet.Text}, {tweet.CreatedBy}");
+        await userClient.Tweets.DestroyTweetAsync(tweet);
+        Console.WriteLine($"Deleted: {tweet.Id}");
+        deletedTweet += 1;
+    }
+    return deletedTweet;
+}
 
-        static async Task<int> CleanMyTimeLine(ITwitterClient userClient)
+static async Task<int> Unfav(ITwitterClient userClient)
+{
+    var user = await userClient.Users.GetAuthenticatedUserAsync();
+    var tweets = await userClient.Tweets.GetUserFavoriteTweetsAsync(user);
+    Console.WriteLine($"Getting tweets: {tweets.Length}");
+    int unfavCount = 0;
+    foreach (var tweet in tweets)
+    {
+        try
         {
-            var user = await userClient.Users.GetAuthenticatedUserAsync();
-            var tweets = await userClient.Timelines.GetUserTimelineAsync(user);
-            Console.WriteLine($"Getting tweets: {tweets.Length}");
-            int deletedTweet = 0;
-            foreach (var tweet in tweets)
-            {
-                if (!string.IsNullOrWhiteSpace(tweet.InReplyToScreenName) && !user.ScreenName.Equals(tweet.InReplyToScreenName))
-                {
-                    Console.WriteLine($"Skip reply to another person: ${tweet.Text}");
-                    continue;
-                }
-                Console.WriteLine($"Tweet: {tweet.Text}, {tweet.CreatedBy}");
-                await userClient.Tweets.DestroyTweetAsync(tweet);
-                Console.WriteLine($"Deleted: {tweet.Id}");
-                deletedTweet += 1;
-            }
-            return deletedTweet;
+            Console.WriteLine($"Tweet: {tweet.Text}, {tweet.CreatedBy}");
+            await userClient.Tweets.UnfavoriteTweetAsync(tweet);
+            Console.WriteLine($"Unfaved: {tweet.Id}");
+            unfavCount += 1;
         }
-
-        static async Task<int> Unfav(ITwitterClient userClient)
+        catch (Exception ex)
         {
-            var user = await userClient.Users.GetAuthenticatedUserAsync();
-            var tweets = await userClient.Tweets.GetUserFavoriteTweetsAsync(user);
-            Console.WriteLine($"Getting tweets: {tweets.Length}");
-            int unfavCount = 0;
-            foreach (var tweet in tweets)
-            {
-                try
-                {
-                    Console.WriteLine($"Tweet: {tweet.Text}, {tweet.CreatedBy}");
-                    await userClient.Tweets.UnfavoriteTweetAsync(tweet);
-                    Console.WriteLine($"Unfaved: {tweet.Id}");
-                    unfavCount += 1;
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
-            return unfavCount;
+            Console.WriteLine(ex.Message);
         }
     }
+    return unfavCount;
 }
